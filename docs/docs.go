84d1ada765fc2ea2011dc495b6a66227ca88c790
José Xavier <josexavier46@outlook.pt>
2025-06-03 02:04:27 +0100

    chore: Update host configuration to use localhost in Swagger files and .gitignore

--- docs/docs.go
+++ docs/docs.go
@@ -444,7 +444,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "gateway.local:8080",
+	Host:             "localhost:8080",
 	BasePath:         "/api",
 	Schemes:          []string{},
 	Title:            "ESP32 Backend API",
